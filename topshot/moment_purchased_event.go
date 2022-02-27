2ff879f53a9e0da2fc1451d35abd76e752ce245e
benn1087 <bhe007@gmail.com>
2022-02-27 17:02:19 -0500

    Update moment_purchased_event.go

--- topshot/moment_purchased_event.go
+++ topshot/moment_purchased_event.go
@@ -1,4 +1,4 @@
-package topshot
+/*package topshot
 
 import (
 	"fmt"
@@ -29,4 +29,39 @@ func (evt MomentPurchasedEvent) Seller() *flow.Address {
 func (evt MomentPurchasedEvent) String() string {
 	return fmt.Sprintf("moment purchased: momentid: %d, price: %f, seller: %s",
 		evt.Id(), evt.Price(), evt.Seller())
+}
+*/
+
+
+package topshot
+
+import (
+	"fmt"
+
+	"github.com/onflow/cadence"
+	"github.com/onflow/flow-go-sdk"
+)
+// pub event MomentMinted(momentID: UInt64, playID: UInt32, setID: UInt32, serialNumber: UInt32)
+type MomentMintedEvent cadence.Event
+
+func (evt MomentMintedEvent) Id() uint64 {
+	return uint64(evt.Fields[0].(cadence.UInt64))
+}
+
+func (evt MomentMintedEvent) PlayId() uint32 {
+	return uint32(evt.Fields[1].(cadence.UInt32))
+}
+
+func (evt MomentMintedEvent) SetId() uint32 {
+	return uint32(evt.Fields[2].(cadence.UInt32))
+}
+
+func (evt MomentMintedEvent) serialNumber() uint32 {
+	return uint32(evt.Fields[3].(cadence.UInt32))
+}
+
+
+func (evt MomentMintedEvent) String() string {
+	return fmt.Sprintf("moment mint4ed: momentid: %d, playid: %d, setid: %d, serialnum: %d",
+		evt.Id(), evt.PlayId(), evt.SetID(), evt.serialNumber())
 }
