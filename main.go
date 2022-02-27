6668f37199e799e3621a1f5840235a895a3b118a
benn1087 <bhe007@gmail.com>
2022-02-26 19:47:49 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,51 +1,59 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/rrrkren/topshot-sales/topshot"
+   "context"
+   "fmt"
+   
+   "github.com/benn1087/topshot-sales/topshot"
 
-	"github.com/onflow/flow-go-sdk/client"
-	"google.golang.org/grpc"
+   "github.com/onflow/flow-go-sdk/client"
+   "google.golang.org/grpc"
 )
-
 func handleErr(err error) {
-	if err != nil {
-		panic(err)
-	}
+   if err != nil {
+      panic(err)
+   }
 }
 
 func main() {
-	// connect to flow
-	flowClient, err := client.New("access.mainnet.nodes.onflow.org:9000", grpc.WithInsecure())
-	handleErr(err)
-	err = flowClient.Ping(context.Background())
-	handleErr(err)
-
-	// fetch latest block
+    flowClient, err := client.New("access.mainnet.nodes.onflow.org:9000", grpc.WithInsecure())
+    handleErr(err)
+    err = flowClient.Ping(context.Background())
+    handleErr(err)
 	latestBlock, err := flowClient.GetLatestBlock(context.Background(), false)
 	handleErr(err)
 	fmt.Println("current height: ", latestBlock.Height)
 
-	// fetch block events of topshot Market.MomentPurchased events for the past 1000 blocks
 	blockEvents, err := flowClient.GetEventsForHeightRange(context.Background(), client.EventRangeQuery{
-		Type:        "A.c1e4f4f4c4257510.Market.MomentPurchased",
-		StartHeight: latestBlock.Height - 500,
+		Type:        "A.c1e4f4f4c4257510.TopShotMarketV3.MomentPurchased",
+		StartHeight: latestBlock.Height-30,
 		EndHeight:   latestBlock.Height,
-	})
-	handleErr(err)
+	 })
+	 handleErr(err)
+
+	 for _, blockEvent := range blockEvents {
+		for _, purchaseEvent := range blockEvent.Events {
+		  fmt.Println(purchaseEvent)
+		  fmt.Println(purchaseEvent.Value)
+		}
+	 }
+
+
+	 
 
 	for _, blockEvent := range blockEvents {
-		for _, purchaseEvent := range blockEvent.Events {
-			// loop through the Market.MomentPurchased events in this blockEvent
-			e := topshot.MomentPurchasedEvent(purchaseEvent.Value)
-			fmt.Println(e)
-			saleMoment, err := topshot.GetSaleMomentFromOwnerAtBlock(flowClient, blockEvent.Height-1, *e.Seller(), e.Id())
-			handleErr(err)
-			fmt.Println(saleMoment)
-			fmt.Printf("transactionID: %s, block height: %d\n",
-				purchaseEvent.TransactionID.String(), blockEvent.Height)
-			fmt.Println()
-		}
+   		if len(blockEvent.Events) != 0 {
+      		for _, purchaseEvent := range blockEvent.Events {
+         		e := topshot.MomentPurchasedEvent(purchaseEvent.Value)
+         		fmt.Println(e)
+				 saleMoment, err := topshot.GetSaleMomentFromOwnerAtBlock(flowClient, blockEvent.Height-1, *e.Seller(), e.Id())
+				 handleErr(err)
+				 fmt.Println(saleMoment)
+				 fmt.Printf("transactionID: %s, block height: %d\n",
+					 purchaseEvent.TransactionID.String(), blockEvent.Height)
+				 fmt.Println()
+      		}
+   		}
 	}
+	 
 }
